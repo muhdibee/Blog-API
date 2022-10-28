913baf663f4fc13ce078848c287709b6b24e1cdc
Muhammad <muhdibee@gmail.com>
2022-10-28 19:20:15 +0100

    Connected server to mongoDB.

---
+++ DBConnection.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const MONGODB_CONNECTION_STRING = process.env.MONGODB_CONNECTION_STRING;
+
+module.exports = function () {
+	mongoose.connect(MONGODB_CONNECTION_STRING);
+	mongoose.connection.on("connected", () => {
+		console.log("Connected to MongoDB Successfully.");
+	});
+	mongoose.connection.on("error", (error) => {
+		console.log("An error ocurred while connecting to MongoDB");
+		console.log("Error: ", error);
+	});
+};
--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+require("dotenv").config();
 const express = require("express");
+const connectToMongoDB = require("./DBConnection");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+//Connect to MongoDB.
+connectToMongoDB();
+
+app.get("/", (req, res) => {
+	res.json({ status: true, message: "Welcome to Blog" });
+});
+
+app.listen(port, () => {
+	console.log(`Server running on port ${port}.`);
+});
